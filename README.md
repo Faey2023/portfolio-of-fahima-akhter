ea2420b98b52402e0eecd132c01394ddd830b8a3
Fahima Akhter <sfahima1388@gmail.com>
2025-05-03 19:34:45 +0600

    readme added with project setup process

---
+++ README.md
@@ -0,0 +1,29 @@
+# Live link
+
+https://portfolio-of-fahima-akhter.vercel.app
+
+## Getting Started
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/Faey2023/portfolio-of-fahima-akhter
+
+   ```
+
+2. **Navigate to the project directory:**
+
+   ```bash
+   cd portfolio-of-fahima-akhter
+   ```
+
+3. **Install dependencies:**
+
+   ```bash
+   npm install
+   ```
+
+4. **Run the project:**
+   ```bash
+   npm run dev
+   ```
--- src/components/Shared/navbar/NavBar.tsx
+++ src/components/Shared/navbar/NavBar.tsx
@@ -28,17 +28,25 @@ const NavBar = () => {
           {navItems.map(({ href, label, icon }) => {
             const isActive = pathname === href;
             return (
-              <li className="group relative" key={label}>
+              <li className="group relative overflow-hidden" key={label}>
                 <Link href={href}>
                   <div className="flex h-10 w-10 items-center rounded-[50px] font-semibold uppercase opacity-0 transition-all duration-300 ease-in-out group-hover:w-32 group-hover:bg-[#800020] group-hover:text-white group-hover:opacity-100">
-                    <p className="group-hover:pl-3.5">{label}</p>
+                    <p className="pl-0 transition-all duration-300 ease-in-out group-hover:pl-3.5">
+                      {label}
+                    </p>
                   </div>
 
                   <div
-                    className={`absolute top-0 right-0 flex h-10 w-10 items-center justify-center rounded-full border-2 text-xl transition-all duration-300 ease-in-out ${isActive ? 'border-[#800020] bg-[#800020]' : 'border-[#800020] bg-[#eeeeee]'} group-hover:bg-[#800020] group-hover:text-white`}
+                    className={`absolute top-0 right-0 z-10 flex h-10 w-10 items-center justify-center rounded-full border-2 text-xl transition-all duration-300 ease-in-out ${
+                      isActive
+                        ? 'border-[#800020] bg-[#800020]'
+                        : 'border-[#800020] bg-[#eeeeee]'
+                    } group-hover:bg-[#800020] group-hover:text-white`}
                   >
                     <span
-                      className={`${isActive ? 'text-white' : 'text-black'} group-hover:text-white`}
+                      className={`${
+                        isActive ? 'text-white' : 'text-black'
+                      } group-hover:text-white`}
                     >
                       {icon}
                     </span>
