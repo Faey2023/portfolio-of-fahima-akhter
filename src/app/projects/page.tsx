57518b060f53a4455d6a55b80483345e6cf2a719
Fahima Akhter <sfahima1388@gmail.com>
2025-05-08 07:55:10 +0600

    card length fixed

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
         <div className="mx-auto mb-12 grid max-w-6xl grid-cols-1 gap-5 px-3 md:grid-cols-2 lg:grid-cols-3 lg:px-0">
           {projectData.map((project: IProject) => (
             <div key={project.id}>
-              <div className="group h-[575px] w-full overflow-hidden rounded-2xl bg-white shadow-lg transition">
+              <div className="group w-full overflow-hidden rounded-2xl bg-white shadow-lg transition">
                 <div className="group relative h-56 w-full overflow-hidden rounded-md">
                   {project.image && (
                     <Image
@@ -42,7 +42,9 @@ const Projects = () => {
                   <h2 className="text-xl font-semibold text-[#800020]">
                     {project.project_title}
                   </h2>
-                  <p className="text-sm text-gray-600">{project.description}</p>
+                  <p className="text-sm text-gray-600">
+                    {project?.description.slice(0, 150)}...
+                  </p>
                   <div className="flex flex-wrap gap-2 text-xs">
                     {project.coreTechnologies.map((tech, index) => (
                       <span
